94f363331c91c8fda9ae90b699c1b83bad7d451f
marzia4@student.ubc.ca <marzia4@student.ubc.ca>
2026-04-08 16:33:58 -0700

    working receiver and controller files

--- BOT_CONTROLLER/BOT_CONTROLLER.ino
+++ BOT_CONTROLLER/BOT_CONTROLLER.ino
@@ -1,118 +1,302 @@
-// Controller BLE connection script
-// Connects to car to send velocity commands and E-stop
+// Test controller script
 
 #include <ArduinoBLE.h>
 #include <Arduino_APDS9960.h>
+#include <Arduino_BMI270_BMM150.h>
+
+const char* deviceServiceUuid = "19b10000-e8f2-537e-4f6c-d104768a1214";
+const char* commandCharacteristicUuid = "19b10001-e8f2-537e-4f6c-d104768a1214";
+const char* velocityCharacteristicUuid = "19b10002-e8f2-537e-4f6c-d104768a1214";
+
+#define BUTTON_PIN 2
+#define LED_PIN LED_BUILTIN
+
+const int ALPHA = 0.97; // Complimentary filter coefficient
+
+// timestep function
+float lastTime = 0;
+
+// complementary filter function
+float accel_x, accel_y, accel_z;
+float gyro_x, gyro_y, gyro_z;
+
+float roll_accel;
+float pitch_accel;
+float pitch_gyro = 0;
+float roll_gyro = 0;
+
+float roll_filter = 0;
+float pitch_filter = 0;
+
+int gesture = -1; // default no gesture detected
+byte command = 1; // default of E-stop activated - motors default off
+
+const unsigned long INTERVAL = 100; // Interval for sending data, in ms
+unsigned long prevData = 0; // track when the last package was sent to ensure under 500ms
+
+// reset for estop and gesture recognition
+unsigned long resetStart = 0;
+bool resetLatched = false;
+
+// define a struct for sending the individual motor speeds
+struct MotorCommand {
+  int leftSpeed;
+  int rightSpeed;
+};
 
 void setup() {
   Serial.begin(9600);
   while (!Serial);
 
-  // initialize the Bluetooth® Low Energy hardware
-  if (!BLE.begin()) {
-    Serial.println("starting Bluetooth® Low Energy module failed!");
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  pinMode(LED_PIN, OUTPUT);
+  
+  if (!APDS.begin()) {
+    Serial.println("* Error initializing APDS9960 sensor!");
+  } 
 
+  APDS.setGestureSensitivity(80); 
+  
+  if (!BLE.begin()) {
+    Serial.println("* Starting Bluetooth® Low Energy module failed!");
     while (1);
   }
-  // Initialize the gesture recognition
-  if (!APDS.begin()) {
-    Serial.println("Error initializing APDS-9960 sensor.");
+
+  // initialize IMU acceleration and gyroscope sensors
+  if (!IMU.begin()){
+    Serial.println("Error initializing IMU sensor.");    
   }
+  
+  BLE.setLocalName("Nano 33 BLE (Central)"); 
 
-  Serial.println("Bluetooth® Low Energy Central - velocity control");
-
-  // start scanning for peripherals
-  BLE.scanForUuid("19b10000-e8f2-537e-4f6c-d104768a1214");
-
+  Serial.println("Arduino Nano 33 BLE Sense (Central Device)");
+  Serial.println(" ");
 }
 
 void loop() {
-  // check if a peripheral has been discovered
-  BLEDevice peripheral = BLE.available();
+  float dt = timestep(millis());
+  connectToPeripheral();
 
+  gestureSteering(dt);
+}
+
+void connectToPeripheral(){
+  BLEDevice peripheral;
+  
+  Serial.println("- Discovering peripheral device...");
+
+  do
+  {
+    BLE.scanForUuid(deviceServiceUuid);
+    peripheral = BLE.available();
+  } while (!peripheral);
+  
   if (peripheral) {
-    // discovered a peripheral, print out address, local name, and advertised service
-    Serial.print("Found ");
-    Serial.print(peripheral.address());
-    Serial.print(" '");
-    Serial.print(peripheral.localName());
-    Serial.print("' ");
-    Serial.print(peripheral.advertisedServiceUuid());
-    Serial.println();
-
-    if (peripheral.localName() != "CAR") {
-      return;
-    }
-
-    // stop scanning
+    Serial.println("* Peripheral device found!");
+    Serial.print("* Device MAC address: ");
+    Serial.println(peripheral.address());
+    Serial.print("* Device name: ");
+    Serial.println(peripheral.localName());
+    Serial.print("* Advertised service UUID: ");
+    Serial.println(peripheral.advertisedServiceUuid());
+    Serial.println(" ");
     BLE.stopScan();
-
-    controlCar(peripheral);
-
-    // peripheral disconnected, start scanning again
-    BLE.scanForUuid("19b10000-e8f2-537e-4f6c-d104768a1214");
+    controlPeripheral(peripheral);
   }
 }
 
-void controlCar(BLEDevice peripheral) {
-  // connect to the peripheral
-  Serial.println("Connecting ...");
+void controlPeripheral(BLEDevice peripheral) {
+  Serial.println("- Connecting to peripheral device...");
 
   if (peripheral.connect()) {
-    Serial.println("Connected");
+    Serial.println("* Connected to peripheral device!");
+    Serial.println(" ");
   } else {
-    Serial.println("Failed to connect!");
+    Serial.println("* Connection to peripheral device failed!");
+    Serial.println(" ");
     return;
   }
 
-  // discover peripheral attributes
-  Serial.println("Discovering attributes ...");
+  Serial.println("- Discovering peripheral device attributes...");
   if (peripheral.discoverAttributes()) {
-    Serial.println("Attributes discovered");
+    Serial.println("* Peripheral device attributes discovered!");
+    Serial.println(" ");
   } else {
-    Serial.println("Attribute discovery failed!");
+    Serial.println("* Peripheral device attributes discovery failed!");
+    Serial.println(" ");
     peripheral.disconnect();
     return;
   }
 
-  // retrieve the velocity characteristic
-  BLECharacteristic velocityCharacteristic = peripheral.characteristic("19b10001-e8f2-537e-4f6c-d104768a1214");
+  BLECharacteristic commandCharacteristic = peripheral.characteristic(commandCharacteristicUuid);
+  BLECharacteristic velocityCharacteristic = peripheral.characteristic(velocityCharacteristicUuid);
 
-  if (!velocityCharacteristic) {
-    Serial.println("Peripheral does not have velocity characteristic!");
-    peripheral.disconnect();
-    return;
-  } else if (!velocityCharacteristic.canWrite()) {
-    Serial.println("Peripheral does not have a writable velocity characteristic!");
+  if (!commandCharacteristic || !commandCharacteristic.canWrite()) {
+    Serial.println("* Invalid command characteristic. Disconnecting...");
     peripheral.disconnect();
     return;
   }
-
+  if (!velocityCharacteristic || !velocityCharacteristic.canWrite()) {
+    Serial.println("* Invalid velocity characteristic. Disconnecting...");
+    peripheral.disconnect();
+    return;
+  }
+  
   while (peripheral.connected()) {
-    // while the peripheral is connected
 
-    // read the button pin
-    int buttonState = digitalRead(buttonPin);
+    BLE.poll();
 
-    if (oldButtonState != buttonState) {
-      // button changed
-      oldButtonState = buttonState;
+    float dt = timestep(millis());
 
-      if (buttonState) {
-        Serial.println("ESTOP pressed");
+    if (detectGesture()) {
+      resetLatched = true;
+      resetStart = millis();
+    }
 
-        // button is pressed, write 0x01 to turn the motors off
-        buttonCharacteristic.writeValue((byte)0x01);
-      } 
-      // change to else if and detect gesture - then reset button state? can that be done?
-      else {
-        Serial.println("ESTOP released");
+    // Check for Estop activation
+    if (digitalRead(BUTTON_PIN) == LOW) {
+      command = 1; // ESTOP
+    }
+    // Check if any gesture has been detected, if so, reset the Estop
+    else if (resetLatched) {
+      command = 2;
 
-        // button is released, write 0x00 to turn the motors back on
-        buttonCharacteristic.writeValue((byte)0x00);
+      if (millis() - resetStart > 300) {
+        resetLatched = false;
       }
     }
+    // If no Estop, run the motors as normal and send velocity commands
+    else {
+      command = 0; // normal operation
+    }
+
+    // send data at the specified time interval
+    if (millis() - prevData > INTERVAL) {
+
+      commandCharacteristic.writeValue(command); // send estop or reset command
+
+      // only send motor speeds when not estopped
+      if (command == 0){
+        MotorCommand velCmd = gestureSteering(dt);
+
+        velocityCharacteristic.writeValue( (byte*)&velCmd, sizeof(velCmd));
+
+        Serial.print("Left: ");
+        Serial.print(velCmd.leftSpeed);
+        Serial.print(" Right: ");
+        Serial.println(velCmd.rightSpeed);
+      }
+
+      prevData = millis();
+    }
+
+    digitalWrite(LED_PIN, LOW); // Keep light on to indicate normal operation
+  }
+  Serial.println("- Peripheral device disconnected!");
+}
+
+// Look for any gesture on the controller to reset the Estop
+bool detectGesture() {
+  if (APDS.gestureAvailable()) {
+    int gesture = APDS.readGesture();
+
+    switch (gesture) {
+      case GESTURE_UP:
+      case GESTURE_DOWN:
+      case GESTURE_LEFT:
+      case GESTURE_RIGHT:
+        Serial.println(" Gesture Found: Starting...");
+        return true;
+      default:
+        Serial.println("No gesture detected");
+        break;
+      }
+    }
+    return false;
+}
+
+// timestep function
+// returns timestep
+float timestep(unsigned long currentTime){
+
+  float dt = (currentTime - lastTime) / 1000;
+  lastTime = currentTime;
+  return dt;
+}
+
+// complementary filter for velocity control
+// returns a roll angle
+float complementaryRoll(float dt){
+  if (IMU.accelerationAvailable()){
+
+    // read x,y,z acceleration from IMU
+    IMU.readAcceleration(accel_x, accel_y, accel_z);
+
+    // calculate roll angle using Arduino math library
+    roll_accel = atan2(accel_y, sqrt(accel_x * accel_x + accel_z * accel_z)) * 180 / PI;
   }
 
-  Serial.println("Peripheral disconnected");
+  if (IMU.gyroscopeAvailable()){
+
+    // read x,y,z gyroscope values from IMU
+    IMU.readGyroscope(gyro_x, gyro_y, gyro_z);
+
+    // new angle = old angle + (angular vel x dt)
+    roll_gyro = roll_gyro + (gyro_x * dt); // changed from y to x
+  }
+ 
+  roll_filter = ALPHA * (roll_filter + gyro_x*dt) + (1-ALPHA) * roll_accel;
+
+  return roll_filter;
 }
+
+// complementary filter for velocity control
+// returns a pitch angle
+float complementaryPitch(float dt){
+  if (IMU.accelerationAvailable()){
+
+    // read x,y,z acceleration from IMU
+    IMU.readAcceleration(accel_x, accel_y, accel_z);
+
+    // calculate pitch angle
+    pitch_accel = atan2(-accel_x, sqrt(accel_y * accel_y + accel_z * accel_z)) * 180 / PI;
+  }
+
+  if (IMU.gyroscopeAvailable()){
+
+    // read x,y,z gyroscope values from IMU
+    IMU.readGyroscope(gyro_x, gyro_y, gyro_z);
+
+    // integrate gyro around Y-axis for pitch
+    pitch_gyro = pitch_gyro + (gyro_y * dt);
+  }
+ 
+  pitch_filter = ALPHA * (pitch_filter + gyro_y*dt) + (1-ALPHA) * pitch_accel;
+
+  return pitch_filter;
+}
+
+// calculates velocities based on roll and pitch pose to send to receiver
+// uses complementaryPitch, complementaryRoll
+MotorCommand gestureSteering(float dt){
+  MotorCommand cmd;
+
+  float pitch_angle = complementaryPitch(dt);
+  float roll_angle = complementaryRoll(dt);
+
+  // deadband to avoid drift
+  if (abs(pitch_angle) < 5) pitch_angle = 0;
+  if (abs(roll_angle) < 5)  roll_angle = 0;
+
+  // map tilt angle to motor speed
+  float forward = map(constrain(pitch_angle, -30, 30), -30, 30, -255, 255);
+  float turn    = map(constrain(roll_angle, -30, 30), -30, 30, -150, 150);
+
+  // differential drive mixing
+  cmd.leftSpeed  = constrain(forward + turn, -255, 255);
+  cmd.rightSpeed = constrain(forward - turn, -255, 255);
+
+  return cmd;
+}
+
+
--- BOT_RECEIVER/BOT_RECEIVER.ino
+++ BOT_RECEIVER/BOT_RECEIVER.ino
@@ -1,63 +1,183 @@
-// BLE Receiver - CAR
-// Receives steering/velocity information from the controller when in manual mode
-// Should eventually send back position and mapping data to build temperature and obstacle map
+// Test receiver script
 
 #include <ArduinoBLE.h>
 
-BLEService carService("19B10010-E8F2-537E-4F6C-D104768A1214"); // create service
+#define IN1 2
+#define IN2 4
+#define ENA A2
+#define IN3 5
+#define IN4 6
+#define ENB A1
 
-// create button characteristic and allow remote device to read and write
-BLEByteCharacteristic buttonCharacteristic("19B10012-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite);
+#define LED_PIN LED_BUILTIN
+      
+enum CarState{
+  RUNNING,
+  ESTOP
+};
 
-// Trial of velocity command characteristic for receiver
-BLEByteCharacteristic velocityCharacteristic("19B10001-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite);
+CarState state = ESTOP; // Default of motors off
+
+const char* deviceServiceUuid = "19b10000-e8f2-537e-4f6c-d104768a1214";
+const char* commandCharacteristicUuid = "19b10001-e8f2-537e-4f6c-d104768a1214";
+const char* velocityCharacteristicUuid = "19b10002-e8f2-537e-4f6c-d104768a1214";
+
+int gesture = -1;
+
+// struct for motor speed commands
+struct MotorCommand {
+  int leftSpeed;
+  int rightSpeed;
+};
+
+MotorCommand currentCommand = {0, 0}; // set default values for motors, off
+
+BLEService gestureService(deviceServiceUuid); 
+BLEByteCharacteristic commandCharacteristic(commandCharacteristicUuid, BLERead | BLEWrite);
+BLECharacteristic velocityCharacteristic(velocityCharacteristicUuid, BLERead | BLEWrite, sizeof(MotorCommand));
+
 
 void setup() {
   Serial.begin(9600);
-  while (!Serial);
+  while (millis()>1000);  // don't wait for serial connection - battery power
+  
+  pinMode(LEDR, OUTPUT);
+  pinMode(LEDG, OUTPUT);
+  pinMode(LEDB, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
 
-  // begin initialization
+  // Motor pins
+  pinMode(IN1, OUTPUT); // Input 1A
+  pinMode(IN2, OUTPUT); // Input 2A
+  pinMode(ENA, OUTPUT); // Enable pin 1/2
+  pinMode(IN3, OUTPUT); // Input 3A
+  pinMode(IN4, OUTPUT); // Input 4A
+  pinMode(ENB, OUTPUT); // Enable pin 3/4
+  
+  stopMotors(); // Default motors to off state
+
+  
   if (!BLE.begin()) {
-    Serial.println("starting Bluetooth® Low Energy module failed!");
-
+    Serial.println("- Starting Bluetooth® Low Energy module failed!");
     while (1);
   }
 
-  // set the local name peripheral advertises
-  BLE.setLocalName("CAR");
-  // set the UUID for the service this peripheral advertises:
-  BLE.setAdvertisedService(carService);
+  BLE.setLocalName("Arduino Nano 33 BLE (Peripheral)");
+  BLE.setAdvertisedService(gestureService);
 
-  // add the characteristics to the service
-  carService.addCharacteristic(buttonCharacteristic);
-  carService.addCharacteristic(velocityCharacteristic);
-
-  // add the service
-  BLE.addService(carService);
-
-  buttonCharacteristic.writeValue(0);
-  velocityCharacteristic.writeValue(0);
-
-  // start advertising
+  gestureService.addCharacteristic(commandCharacteristic);
+  gestureService.addCharacteristic(velocityCharacteristic);
+  BLE.addService(gestureService);
+  commandCharacteristic.writeValue(1); // Default to 1 for ESTOP activation at start
   BLE.advertise();
 
-  Serial.println("Bluetooth® device active, waiting for connections...");
+  Serial.println("Nano 33 BLE (Peripheral Device)");
+  Serial.println(" ");
 }
 
 void loop() {
-  // poll for Bluetooth® Low Energy events
-  BLE.poll();
+  BLEDevice central = BLE.central();
+  Serial.println("- Discovering central device...");
 
-  // Check if the E stop was activated
-  if (buttonCharacteristic.value() == 1) {
-    // button state changed, update characteristics
-    buttonCharacteristic.writeValue(buttonValue);
+  if (central) {
+    Serial.println("* Connected to central device!");
 
-    // Turn everything off - analogwrite low to motor pins
+    while (central.connected()) {
+      BLE.poll();
+
+      if (commandCharacteristic.written()) {
+        byte cmd = commandCharacteristic.value();
+
+        Serial.print("New CMD: ");
+        Serial.println(cmd);
+
+        if (cmd == 1) {
+          state = ESTOP;
+        }
+        else if (cmd == 2 && state == ESTOP) {
+          state = RUNNING;
+          Serial.println("Reset received: RUNNING");
+        }
+      }
+
+      //velocity control
+      if (velocityCharacteristic.written()) {
+        velocityCharacteristic.readValue((byte*)&currentCommand,sizeof(currentCommand));
+
+        Serial.print("Left speed: ");
+        Serial.print(currentCommand.leftSpeed);
+        Serial.print(" Right speed: ");
+        Serial.println(currentCommand.rightSpeed);
+      }
+
+      // Check if the Estop has been activated
+      if (state == ESTOP) {
+        stopMotors();
+
+        // RED LED = E-stop
+        digitalWrite(LEDR, LOW);
+        digitalWrite(LEDG, HIGH);
+        digitalWrite(LEDB, HIGH);
+      }
+      else {
+        runMotors(currentCommand.leftSpeed, currentCommand.rightSpeed);
+
+        // GREEN LED = running
+        digitalWrite(LEDR, HIGH);
+        digitalWrite(LEDG, LOW);
+        digitalWrite(LEDB, HIGH);
+      }
+    }
+    
 
   }
+  if (!central) {
+  state = ESTOP;
+  stopMotors();
 
-  // continuously read the velocity data to send commands to the motors
-  // figure out whether to do the math for either motor here or in the controller circuit
+    // Blink red LED continuously while disconnected
+    if (millis() % 200 < 100) {
+      digitalWrite(LEDR, LOW);   // ON
+    } else {
+      digitalWrite(LEDR, HIGH);  // OFF
+    }
+
+    digitalWrite(LEDG, HIGH);
+    digitalWrite(LEDB, HIGH);
+  }
+}
+
+// Script to stop motors when bluetooth disconnected or Estop activated
+// Sets motor speeds to zero
+void stopMotors(){
+  // set pwm pins to 0 to stop them
+  analogWrite(ENA, 0); 
+  analogWrite(ENB, 0); 
+}
+
+// Run the motors with generic values for now
+void runMotors(int leftSpeed, int rightSpeed){
+
+  // LEFT MOTOR
+  if (leftSpeed >= 0) {
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    analogWrite(ENA, leftSpeed);
+  } else {
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    analogWrite(ENA, abs(leftSpeed));
+  }
+
+  // RIGHT MOTOR
+  if (rightSpeed >= 0) {
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+    analogWrite(ENB, rightSpeed);
+  } else {
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+    analogWrite(ENB, abs(rightSpeed));
+  }
 
 }
